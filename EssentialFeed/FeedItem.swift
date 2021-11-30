1ba656d8f90f683e327b4239bbc081658836f36b
vchauhan <vchauhan@willowpump.com>
2021-12-01 00:21:31 +0530

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Viral on 01/12/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: String
+}
