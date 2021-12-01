8a09a9edd283b1a86dda67f3717ae3075f7f4369
vchauhan <vchauhan@willowpump.com>
2021-12-01 20:16:22 +0530

    combine Spy params into single instance

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -53,15 +53,18 @@ class RemoteFeedLoaderTest: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        var completions = [(Error) -> Void]()
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
+
+        private var messages = [(url: URL, completion: (Error) -> Void)]()
+
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            completions.append(completion)
-            requestedURLs.append(url)
+            messages.append((url, completion))
         }
 
         func complete(with error: Error, at index: Int = 0) {
-            completions[index](error)
+            messages[index].completion(error)
         }
     }
 }
