d1667e602de5cbd561cb2a2deb01e2358cbf21c2
vchauhan <vchauhan@willowpump.com>
2021-12-01 13:01:01 +0530

    Create separate file for RemoteFeedLoader

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Viral on 01/12/21.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+
+    public func load() {
+        client.get(from: url)
+    }
+}
