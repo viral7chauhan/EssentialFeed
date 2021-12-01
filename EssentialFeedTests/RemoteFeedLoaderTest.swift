995b65100320569fd801be52353b3beb387084d4
vchauhan <vchauhan@willowpump.com>
2021-12-01 13:02:57 +0530

    Improved test method name

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -17,7 +17,7 @@ class RemoteFeedLoaderTest: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
 
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-another-url.com")!
         let (sut, client) = makeSUT(url: url)
         sut.load()
