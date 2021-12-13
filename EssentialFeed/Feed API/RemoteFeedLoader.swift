177ddf8f9d2557c7bac7651e2d5024c720c4b3a0
vchauhan <vchauhan@willowpump.com>
2021-12-13 13:37:40 +0530

    Move Decodable login to private item struct to decouple the feed feature module from API implementation details

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -42,7 +42,9 @@ public final class RemoteFeedLoader {
                     if response.statusCode == 200,
                        let root = try?
                         JSONDecoder().decode(Root.self, from: data) {
-                        completion(.success(root.items))
+                        completion(.success(root.items.map {
+                            $0.item
+                        }))
                     } else {
                         completion(.failure(.invalidData))
                     }
@@ -53,6 +55,21 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable, Equatable {
-    let items: [FeedItem]
+private struct Root: Decodable {
+    let items: [Item]
+}
+
+private struct Item: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+
+    var item: FeedItem {
+        return FeedItem(id: id,
+                        description: description,
+                        location: location,
+                        imageURL: image
+        )
+    }
 }
