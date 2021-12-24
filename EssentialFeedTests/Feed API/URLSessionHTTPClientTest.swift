b780c4fad3bf944d7cc01bdd032cc4a7e2d12af9
viral7chauhan <viral7chauhan@yahoo.com>
2021-12-25 00:17:32 +0530

    Prevent code from data race condition

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -180,7 +180,6 @@ class URLSessionHTTPClientTest: XCTestCase {
         }
 
         override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
             return true
         }
 
@@ -189,6 +188,11 @@ class URLSessionHTTPClientTest: XCTestCase {
         }
 
         override func startLoading() {
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return requestObserver(request)
+            }
+
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
