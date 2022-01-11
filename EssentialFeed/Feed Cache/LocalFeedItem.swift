b36fb09bb04ac007de69fa7739729cec729125f4
viral7chauhan <viral7chauhan@yahoo.com>
2022-01-11 17:49:06 +0530

    Move `LocalFeedItem` on its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,27 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Viral on 11/01/22.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+
+    public init(
+        id: UUID,
+        description: String?,
+        location: String?,
+        imageURL: URL
+    ) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
