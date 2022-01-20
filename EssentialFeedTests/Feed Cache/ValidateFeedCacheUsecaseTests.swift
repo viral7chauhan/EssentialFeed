80e452968ccc0817eeb01f2812107e44c70e8d58
viral7chauhan <viral7chauhan@yahoo.com>
2022-01-20 14:35:32 +0530

    Does not delete cache on message creation (Validate feed cache context)

---
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUsecaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  ValidateFeedCacheUsecaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Viral on 20/01/22.
+//
+
+import XCTest
+import EssentialFeed
+
+class ValidateFeedCacheUsecaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteOnMessageCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.receivedMsg, [])
+    }
+
+    // MARK: - Helper
+
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (localFeedLoader: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
