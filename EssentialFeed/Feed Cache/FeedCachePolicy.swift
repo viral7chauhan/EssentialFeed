7359377955f2c0c2a4adbf50e872043fbf051d19
viral7chauhan <viral7chauhan@yahoo.com>
2022-01-22 11:28:57 +0530

    Make `FeedCachePolicy` init private

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 struct FeedCachePolicy {
+    private init() {}
+    
     private static let calendar = Calendar(identifier: .gregorian)
 
     private static var maxCacheAgeInDays: Int {
