cdb24b93d0aeab03ce547276b1e67629d070531a
viral7chauhan <viral7chauhan@yahoo.com>
2022-02-02 00:12:03 +0530

    `CoreDataFeedStore.delete()` no error on empty cache

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -55,7 +55,7 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_delete_deliversNoErrorOnEmptyCache() {
         let sut = makeSUT()
 
-        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
 
     func test_delete_hasNoSideEffectsOnEmptyCache() {
