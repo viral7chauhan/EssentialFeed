f0f7f652402b48aa13a3ab1a0a3279aa53870bd5
viral7chauhan <viral7chauhan@yahoo.com>
2022-03-08 12:07:03 +0530

    Drop support of CodableFeedStore - Removed CodableStore file from production bundle - Removed associated test file

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -92,3 +92,12 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
 }
 
+extension NSManagedObject {
+
+    convenience init(context: NSManagedObjectContext) {
+        let name = String(describing: type(of: self))
+        let entity = NSEntityDescription.entity(forEntityName: name, in: context)!
+        self.init(entity: entity, insertInto: context)
+    }
+
+}
