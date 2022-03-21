3ae2fe45c29b1043d607dc2bc1fc41442095e666
viral7chauhan <viral7chauhan@yahoo.com>
2022-03-22 00:38:10 +0530

    Create MacOS Framework and deleted old MacOS Target as `EssentialFeed`

---
+++ EssentialFeed/EssentialFeedMacFramework.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedMacFramework.h
+//  EssentialFeedMacFramework
+//
+//  Created by Viral on 21/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedMacFramework.
+FOUNDATION_EXPORT double EssentialFeedMacFrameworkVersionNumber;
+
+//! Project version string for EssentialFeedMacFramework.
+FOUNDATION_EXPORT const unsigned char EssentialFeedMacFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedMacFramework/PublicHeader.h>
+
+
