4f6141f994d6fe97d409c10c1f31bba2a87e874d
viral7chauhan <viral7chauhan@yahoo.com>
2022-06-20 17:21:09 +0530

    Move `FeedLoaderPresentationAdapter` to separate file

---
+++ EssentialFeediOS/Feed UI/Composers/FeedLoaderPresentationAdaapter.swift
@@ -0,0 +1,34 @@
+//
+//  FeedLoaderPresentationAdaapter.swift
+//  EssentialFeediOS
+//
+//  Created by Viral on 20/06/22.
+//
+
+import EssentialFeed
+
+final class FeedLoaderPresentationAdaapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+
+    func didRequestFeedRefresh() {
+        loadFeed()
+    }
+
+    private func loadFeed() {
+        presenter?.didStartLoadingFeed()
+        feedLoader.load { [weak self] result in
+            switch result {
+                case let .success(feed):
+                    self?.presenter?.didFinishLoadingFeed(with: feed)
+
+                case let .failure(error):
+                    self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -61,29 +61,3 @@ private final class FeedViewAdapter: FeedView {
         }
     }
 }
-
-private final class FeedLoaderPresentationAdaapter: FeedViewControllerDelegate {
-    private let feedLoader: FeedLoader
-    var presenter: FeedPresenter?
-
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-
-    func didRequestFeedRefresh() {
-        loadFeed()
-    }
-
-    private func loadFeed() {
-        presenter?.didStartLoadingFeed()
-        feedLoader.load { [weak self] result in
-            switch result {
-                case let .success(feed):
-                    self?.presenter?.didFinishLoadingFeed(with: feed)
-
-                case let .failure(error):
-                    self?.presenter?.didFinishLoadingFeed(with: error)
-            }
-        }
-    }
-}
