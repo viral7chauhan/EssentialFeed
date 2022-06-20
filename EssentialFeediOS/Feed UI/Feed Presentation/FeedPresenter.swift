65267b821e9a9c75805464dcb8f7d28badd435f9
viral7chauhan <viral7chauhan@yahoo.com>
2022-06-20 09:45:58 +0530

    Move main thread logic into Presenter

--- EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -27,22 +27,37 @@ final class FeedPresenter {
     }
 
     static var title: String {
-        return NSLocalizedString("FEED_VIEW_TITLE",
+        return NSLocalizedString("FEED_VIEW_TITLE", 
             tableName: "Feed",
             bundle: Bundle(for: FeedPresenter.self),
             comment: "Title for the feed view")
     }
 
     func didStartLoadingFeed() {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { [weak self] in
+                self?.didStartLoadingFeed()
+            }
+        }
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
 
     func didFinishLoadingFeed(with feed: [FeedImage]) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { [weak self] in
+                self?.didFinishLoadingFeed(with: feed)
+            }
+        }
         feedView.display(FeedViewModel(feed: feed))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 
     func didFinishLoadingFeed(with error: Error) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { [weak self] in
+                self?.didFinishLoadingFeed(with: error)
+            }
+        }
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
