af43e14c2a5bc902b3d9dbfef14ea8e2f8e597e8
viral7chauhan <viral7chauhan@yahoo.com>
2022-08-15 23:57:59 +0530

    Init `Data` creation

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -86,9 +86,8 @@ class FeedImagePresenterTests: XCTestCase {
     func test_didFinishLoadingImageData_displayRetryOnFailedImageTransformation() {
         let (sut, view) = makeSUT(imageTransformer: fail)
         let image = uniqueImage()
-        let data = Data()
 
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
 
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)
@@ -101,11 +100,10 @@ class FeedImagePresenterTests: XCTestCase {
 
     func test_didFinishLoadingImageData_displaysImageOnSuccessfulTransformation() {
         let image = uniqueImage()
-        let data = Data()
         let transformedData = AnyImage()
         let (sut, view) = makeSUT { _ in transformedData }
 
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
 
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)
