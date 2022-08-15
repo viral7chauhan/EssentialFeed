d759c8d7c02c247e1c07336bcc11ddfc322bee3c
viral7chauhan <viral7chauhan@yahoo.com>
2022-08-15 17:37:57 +0530

    Make System under test factory helper method for test

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -15,12 +15,19 @@ class FeedImagePresenter {
 
 class FeedImagePresenterTests: XCTestCase {
     func test_init_doesNotSendMessageToView() {
-        let view = ViewSpy()
-        _ = FeedImagePresenter(view: view)
+        let (_, view) = makeSUT()
+
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
 
     // MARK: - Helper
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
 
     private class ViewSpy {
         let messages = [Any]()
