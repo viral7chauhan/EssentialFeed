a342348b5fc6730eb2bb743222c003fb4f01b8c0
viral7chauhan <viral7chauhan@yahoo.com>
2022-09-27 00:06:59 +0530

    Rename test case class to clarify intent

--- EssentialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-final class LocalFeedImageDataLoaderTests: XCTestCase {
+final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
 
     func test_init_doesNotMessageStoreUponCreation() {
         let (_ ,store) = makeSUT()
