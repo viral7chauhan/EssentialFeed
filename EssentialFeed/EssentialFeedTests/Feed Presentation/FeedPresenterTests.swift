88ccd9e29ef61d73bb72c469079749cc0d5912c3
viral7chauhan <viral7chauhan@yahoo.com>
2022-12-16 00:45:43 +0530

    Move then GENERIC_CONNECTION_ERROR localisation key to new shared strings

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -49,7 +49,7 @@ class FeedPresenterTests: XCTestCase {
         sut.didFinishLoadingFeed(with: anyNSError())
 
         XCTAssertEqual(view.messages, [
-            .display(errorMessage: localized("GENERIC_CONNECTION_ERROR")),
+            .display(errorMessage: localized("GENERIC_CONNECTION_ERROR", table: "Shared")),
             .display(isLoading: false)
         ])
     }
@@ -63,10 +63,9 @@ class FeedPresenterTests: XCTestCase {
         return (sut, view)
     }
 
-    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-        let table = "Feed"
+    private func localized(_ key: String, table: String = "Feed", file: StaticString = #file, line: UInt = #line) -> String {
         let bundle = Bundle(for: FeedPresenter.self)
-        let value = bundle.localizedString(forKey: key, value: nil, table: "Feed")
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
             XCTFail("Missing localized string for key \(key) in table \(table)", file: file, line: line)
         }
--- EssentialFeed/EssentialFeedTests/Shared Presenter/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Presenter/LoadResourcePresenterTests.swift
@@ -64,9 +64,9 @@ final class LoadResourcePresenterTests: XCTestCase {
     }
 
     private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-        let table = "Feed"
+        let table = "Shared"
         let bundle = Bundle(for: SUT.self)
-        let value = bundle.localizedString(forKey: key, value: nil, table: "Feed")
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
             XCTFail("Missing localized string for key \(key) in table \(table)", file: file, line: line)
         }
