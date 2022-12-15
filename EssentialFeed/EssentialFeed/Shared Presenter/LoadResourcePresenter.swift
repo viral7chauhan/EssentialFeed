2d40092d0b24230cbcac8eee79c0e6133e941f22
viral7chauhan <viral7chauhan@yahoo.com>
2022-12-16 00:16:38 +0530

    Make LoadResourcePresenter generic over the Resource types

--- EssentialFeed/EssentialFeed/Shared Presenter/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presenter/LoadResourcePresenter.swift
@@ -7,20 +7,22 @@
 
 import Foundation
 
-
 public protocol ResourceView {
-    func display(_ viewModel: String)
+    associatedtype ResourceViewModel
+
+    func display(_ viewModel: ResourceViewModel)
 }
 
 
-public final class LoadResourcePresenter {
-    public typealias Mapper = (String) -> String
+public final class LoadResourcePresenter<Resource, View: ResourceView> {
+    public typealias Mapper = (Resource) -> View.ResourceViewModel
 
+    private var resourceView: View
     private var errorView: FeedErrorView
     private var loadingView: FeedLoadingView
-    private var resourceView: ResourceView
     private let mapper: Mapper
-    public init(resourceView: ResourceView, loadingView: FeedLoadingView, errorView: FeedErrorView,
+
+    public init(resourceView: View, loadingView: FeedLoadingView, errorView: FeedErrorView,
                 mapper: @escaping Mapper) {
         self.errorView = errorView
         self.loadingView = loadingView
@@ -41,7 +43,7 @@ public final class LoadResourcePresenter {
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
 
-    public func didFinishLoading(with resource: String) {
+    public func didFinishLoading(with resource: Resource) {
         resourceView.display(mapper(resource))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
