e8da0d4dc31ade68e53af53b2dfb6132fc921351
viral7chauhan <viral7chauhan@yahoo.com>
2022-12-18 20:21:01 +0530

    Replace custom CellController protocol with a composition of the UITableViewDataSource/Delegate/Prefetching, so we can decouple other modules from the Shared UI module

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -13,21 +13,26 @@ public protocol FeedImageCellControllerDelegate {
     func didCancelImageRequest()
 }
 
-public final class FeedImageCellController: CellController, ResourceView, ResourceLoadingView, ResourceErrorView {
+public final class FeedImageCellController: NSObject {
 
     public typealias ResourceViewModel = UIImage
 
     private let viewModel: FeedImageViewModel
-
     private let delegate: FeedImageCellControllerDelegate
-    var cell: FeedImageCell?
+    private var cell: FeedImageCell?
 
     public init(viewModel: FeedImageViewModel, delegate: FeedImageCellControllerDelegate) {
         self.viewModel = viewModel
         self.delegate = delegate
     }
+}
 
-    public func view(in tableView: UITableView) -> UITableViewCell {
+extension FeedImageCellController: CellController {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         cell = tableView.dequeueReusableCell()
         cell?.locationContainer.isHidden = !viewModel.hasLocation
         cell?.locationLabel.text = viewModel.location
@@ -38,7 +43,15 @@ public final class FeedImageCellController: CellController, ResourceView, Resour
         return cell!
     }
 
-    public func preload() {
+    public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        cancelLoad()
+    }
+
+    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cancelLoad()
+    }
+
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         delegate.didRequestImage()
     }
 
@@ -46,7 +59,14 @@ public final class FeedImageCellController: CellController, ResourceView, Resour
         releaseCellForReuse()
         delegate.didCancelImageRequest()
     }
-    
+
+    private func releaseCellForReuse() {
+        cell = nil
+    }
+}
+
+extension FeedImageCellController: ResourceView, ResourceLoadingView, ResourceErrorView {
+
     public func display(_ viewModel: ResourceLoadingViewModel) {
         cell?.feedImageContainer.isShimmering = viewModel.isLoading
     }
@@ -57,9 +77,5 @@ public final class FeedImageCellController: CellController, ResourceView, Resour
 
     public func display(_ viewModel: UIImage) {
         cell?.feedImageView.setImageAnimated(viewModel)
-    }
-
-    private func releaseCellForReuse() {
-        cell = nil
     }
 }
--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -8,18 +8,28 @@
 import UIKit
 import EssentialFeed
 
-public class ImageCommentCellController: CellController {
+public class ImageCommentCellController: NSObject {
     private let model: ImageCommentViewModel
 
     public init(model: ImageCommentViewModel) {
         self.model = model
     }
+}
 
-    public func view(in tableView: UITableView) -> UITableViewCell {
+extension ImageCommentCellController: CellController {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: ImageCommentCell = tableView.dequeueReusableCell()
         cell.messageLabel.text = model.message
         cell.usernameLabel.text = model.username
         cell.dateLabel.text = model.date
         return cell
+    }
+
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        
     }
 }
