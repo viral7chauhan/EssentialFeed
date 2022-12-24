88d34d8d95b7fcce155f410f6b65c4c55473ed7b
viral7chauhan <viral7chauhan@yahoo.com>
2022-12-25 01:01:47 +0530

    Duplicate FeedUIComposer as CommentsUIComposer

---
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -0,0 +1,45 @@
+//
+//  CommentsUIComposer.swift
+//  EssentialApp
+//
+//  Created by Viral on 25/12/22.
+//
+
+import EssentialFeediOS
+import EssentialFeed
+import UIKit
+import Combine
+
+public final class CommentsUIComposer {
+    private init() {}
+
+    private typealias FeedPresentationAdapter =
+    LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
+
+    public static func commentsComposeWith(
+       commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>) -> ListViewController {
+
+        let presentationAdapter = FeedPresentationAdapter(loader: commentsLoader)
+
+        let feedController = makeFeedViewController(title: FeedPresenter.title)
+        feedController.onRefresh = presentationAdapter.loadResource
+
+        presentationAdapter.presenter = LoadResourcePresenter(
+            resourceView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: { _ in Empty<Data, Error>().eraseToAnyPublisher() } ),
+            loadingView: WeakRefVirualProxy(feedController),
+            errorView: WeakRefVirualProxy(feedController),
+            mapper: FeedPresenter.map)
+
+        return feedController
+    }
+
+    private static func makeFeedViewController(title: String) -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+        feedController.title = title
+        return feedController
+    }
+}
---
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -0,0 +1,140 @@
+//
+//  CommentsUIIntegrationTests.swift
+//  EssentialAppTests
+//
+//  Created by Viral on 25/12/22.
+//
+
+import XCTest
+import UIKit
+import EssentialApp
+import EssentialFeed
+import EssentialFeediOS
+
+final class CommentsUIIntegrationTests: FeedUIIntegrationTests {
+
+    override func test_feedView_hasTitle() {
+        let (sut, _) = makeSUT()
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(sut.title, feedTitle)
+    }
+
+
+    override func test_loadFeedActions_requestFeedFromLoader() {
+        let (sut, loader) = makeSUT()
+        XCTAssertEqual(loader.loadFeedCallCount, 0, "Expected no loading requests before view is loaded")
+
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadFeedCallCount, 1, "Expected a loading requests once view is loaded")
+
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(loader.loadFeedCallCount, 2, "Expected another loading request once user initiated a reload")
+
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(loader.loadFeedCallCount, 3, "Expected yet another loading request once user initiated another reload")
+    }
+
+    override func test_loadingFeedIndicator_isVisibleWhileLoadingFeed() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        XCTAssertTrue(sut.isShowLoadingIndicator, "Expected loading indicator once view is loaded")
+
+        loader.completeFeedLoading(at: 0)
+        XCTAssertFalse(sut.isShowLoadingIndicator, "Expected no loading indicator once loading completes successfully")
+
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertTrue(sut.isShowLoadingIndicator, "Expected loading indicator once user initiated a reload")
+
+        loader.completeFeedLoadingWithError(at: 1)
+        XCTAssertFalse(sut.isShowLoadingIndicator, "Expected no loading indicator once user initiated loading completes with error")
+    }
+
+    override func test_loadFeedCompletion_rendersSuccessfullyLoadedFeed() {
+        let feedImage0 = makeImage(description: "a description", location: "a location")
+        let feedImage1 = makeImage(description: nil, location: "another location")
+        let feedImage2 = makeImage(description: "another description", location: nil)
+        let feedImage3 = makeImage(description: nil, location: nil)
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        assertThat(sut: sut, isRendering: [])
+
+        loader.completeFeedLoading(with : [feedImage0], at: 0)
+        assertThat(sut: sut, isRendering: [feedImage0])
+
+        sut.simulateUserInitiatedFeedReload()
+        loader.completeFeedLoading(with: [feedImage0, feedImage1, feedImage2, feedImage3], at: 1)
+        assertThat(sut: sut, isRendering: [feedImage0, feedImage1, feedImage2, feedImage3])
+    }
+
+    override func test_loadFeedCompletion_rendersSuccessfullyLoadedEmptyFeedAfterNonEmptyFeed() {
+        let image0 = makeImage()
+        let image1 = makeImage()
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with : [image0, image1], at: 0)
+        assertThat(sut: sut, isRendering: [image0, image1])
+
+        sut.simulateUserInitiatedFeedReload()
+        loader.completeFeedLoading(with: [], at: 1)
+        assertThat(sut: sut, isRendering: [])
+    }
+
+    override func test_loadFeedCompletion_doesNotAlterCurrentRenderingStateOnError() {
+        let image0 = makeImage()
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with : [image0], at: 0)
+        assertThat(sut: sut, isRendering: [image0])
+
+        sut.simulateUserInitiatedFeedReload()
+        loader.completeFeedLoadingWithError(at: 1)
+        assertThat(sut: sut, isRendering: [image0])
+    }
+
+    override func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertNil(sut.errorMessage)
+
+        loader.completeFeedLoadingWithError(at: 0)
+        XCTAssertEqual(sut.errorMessage, loadError)
+
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertNil(sut.errorMessage)
+    }
+
+    override func test_tapOnErrorView_hidesErrorMessage() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        XCTAssertNil(sut.errorMessage)
+
+        loader.completeFeedLoadingWithError(at: 0)
+        XCTAssertEqual(sut.errorMessage, loadError)
+
+        sut.simulateErrorViewTap()
+        XCTAssertNil(sut.errorMessage)
+    }
+
+    // MARK: - Helper
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ListViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = CommentsUIComposer.commentsComposeWith(commentsLoader: loader.loadPublisher)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, loader)
+    }
+
+    private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "https://a-url.com")!) -> FeedImage {
+        FeedImage(id: UUID(), description: description, location: location, url: url)
+    }
+}
