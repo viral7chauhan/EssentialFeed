611b24443dcfee8ce3cf907b11e2d5024ea830e0
viral <viral7chauhan@yahoo.com>
2023-01-29 00:54:43 +0530

    Merge CellController initializers

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/CellController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/CellController.swift
@@ -13,18 +13,11 @@ public struct CellController {
     let delegate: UITableViewDelegate?
     let prefetch: UITableViewDataSourcePrefetching?
 
-    public init(id: AnyHashable, _ dataSource: UITableViewDataSource & UITableViewDelegate & UITableViewDataSourcePrefetching) {
-        self.id = id
-        self.dataSource = dataSource
-        self.delegate = dataSource
-        self.prefetch = dataSource
-    }
-
     public init(id: AnyHashable, _ dataSource: UITableViewDataSource) {
         self.id = id
         self.dataSource = dataSource
-        self.delegate = nil
-        self.prefetch = nil
+        self.delegate = dataSource as? UITableViewDelegate
+        self.prefetch = dataSource as? UITableViewDataSourcePrefetching
     }
 }
 
