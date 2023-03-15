9284e63f3ff410d951de8132128948c0dba9ff55
viral7chauhan <viral7chauhan@yahoo.com>
2023-03-16 01:20:56 +0530

    Fetch current items from cache when needed instead of keeping them in memory all the time

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -96,15 +96,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	
 	private func makePage(items: [FeedImage], last: FeedImage?) -> Paginated<FeedImage> {
 		Paginated(items: items, loadMorePublisher: last.map { last in
-			{ self.makeRemoteLoadMoreLoader(items: items, last: last) }
+			{ self.makeRemoteLoadMoreLoader(last: last) }
 		})
 	}
 
-	private func makeRemoteLoadMoreLoader( items: [FeedImage], last: FeedImage?)
+	private func makeRemoteLoadMoreLoader(last: FeedImage?)
 	-> AnyPublisher<Paginated<FeedImage>, Error> {
-		makeRemoteFeedLoader(after: last)
-			.map { newItems in
-				return (items + newItems, newItems.last)
+		localFeedLoader.loadPublisher()
+			.zip(makeRemoteFeedLoader(after: last))
+			.map { cachedItems, newItems in
+				(cachedItems + newItems, newItems.last)
 			}.map(makePage)
 			.caching(to: localFeedLoader)
 	}
