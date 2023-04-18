fb429bcde96b87218e1a8d864b5cb24ee6f463cf
viral7chauhan <viral7chauhan@yahoo.com>
2023-04-19 01:19:53 +0530

    Rename method to clarify intent

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -34,7 +34,7 @@ public final class CoreDataFeedStore {
         }
     }
 
-    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+    func performAsync(_ action: @escaping (NSManagedObjectContext) -> Void) {
         let context = self.context
         context.perform { action(context) }
     }
